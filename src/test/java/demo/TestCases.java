43f17a0bdfdc70dff656386b40b9f40a36803711
Charmi <charmi1096@gmail.com>
2024-06-24 12:20:26 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,6 +1,12 @@
 package demo;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,16 +16,8 @@ import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-
-
-// import io.github.bonigarcia.wdm.WebDriverManager;
-import demo.wrappers.Wrappers;
-
 public class TestCases {
-    ChromeDriver driver;
+   static  ChromeDriver driver;
 
     /*
      * TODO: Write your tests here with testng @Test annotation. 
@@ -53,6 +51,136 @@ public class TestCases {
         driver.manage().window().maximize();
     }
 
+    @Test
+    public static void testCase01() throws InterruptedException
+    {
+        System.out.println("Start Test case: testCase01");
+        //1.Go to www.flipkart.com
+        driver.get("https://www.flipkart.com/");
+        Thread.sleep(3000);  
+        //2. Search "Washing Machine".
+        WebElement searchBox = driver.findElement(By.xpath("//input[@placeholder='Search for Products, Brands and More']"));
+        searchBox.clear();
+        searchBox.sendKeys("Washing Machine");
+        searchBox.sendKeys(Keys.ENTER);
+        Thread.sleep(3000);
+        //3. Sort by popularity and print the count of items with rating less than or equal to 4 stars.
+        WebElement sortByElement = driver.findElement(By.xpath("//div[text()='Popularity']"));
+        sortByElement.click();
+        Thread.sleep(3000);
+        List<WebElement> items = driver.findElements(By.xpath("//div[@class='_75nlfW']"));
+        int itemSize = items.size();
+        System.out.println("totals item is : "+itemSize);
+        List<WebElement> ratinG = driver.findElements(By.xpath("//div[@class='XQDdHH']"));
+        List<String> ratings = new ArrayList<String>();
+        for (WebElement e : ratinG)
+        {
+            ratings.add(e.getText());
+        }
+        List<String> newRating = new ArrayList<String>(ratings);
+        int count = 0;
+        for(int i = 0; i < newRating.size(); i++) 
+        {
+            
+            if(i<=4){
+              //System.out.println("washing machine by rating :"+newRating.get(i));  
+              count++;
+            }
+        }
+        System.out.println("count of items with rating less than or equal to 4 stars is : "+count);
+        System.out.println("End Test case: testCase01");
+    }
+
+    @Test
+    public static void testCase02() throws InterruptedException
+    {
+        System.out.println("Start Test case: testCase02");
+        //1.Go to www.flipkart.com
+        driver.get("https://www.flipkart.com/");
+        Thread.sleep(3000);  
+        //2. Search "iPhone".
+        WebElement searchBox = driver.findElement(By.xpath("//input[@placeholder='Search for Products, Brands and More']"));
+        searchBox.clear();
+        searchBox.sendKeys("iPhone");
+        searchBox.sendKeys(Keys.ENTER);
+        Thread.sleep(3000);  
+
+        List<WebElement> iphoneDetail = driver.findElements(By.xpath("//div[@class='yKfJKb row']"));
+        
+        List<WebElement> titleOfIphone = driver.findElements(By.xpath("//div[@class='KzDlHZ']"));
+        List<String> title = new  ArrayList<String>();
+        for (WebElement e : titleOfIphone)
+        {
+            title.add(e.getText());
+        }
+        List<String> newTitle = new ArrayList<String>(title);
+        List<WebElement> discount = driver.findElements(By.xpath("//div[@class='UkUFwK']"));
+        List<String> dis = new ArrayList<String>();
+        for (WebElement e : discount)
+        {
+            dis.add(e.getText());
+        }
+        List<String> newDiscount = new ArrayList<String>(dis);
+
+            for(int j = 0; j < 24; j++){
+                
+                if(j>17 && j < 100)
+                   System.out.println("title is : "+newTitle.get(j)+"discount is : "+j+"% off");
+                    
+                
+             
+            }
+    
+        System.out.println("End Test case: testCase02");
+    }
+
+    @Test
+    public static void testCase03() throws InterruptedException
+    {
+        System.out.println("Start Test case: testCase03");
+        driver.get("https://www.flipkart.com/");
+        Thread.sleep(3000);  
+        WebElement searchBox = driver.findElement(By.xpath("//input[@placeholder='Search for Products, Brands and More']"));
+        searchBox.clear();
+        searchBox.sendKeys("Coffee Mug");
+        searchBox.sendKeys(Keys.ENTER);
+        Thread.sleep(3000);  
+        WebElement selectStar = driver.findElement(By.xpath("//*[@id=\"container\"]/div/div[3]/div[1]/div[1]/div/div/div/section[5]/div[2]/div/div[1]/div/label"));
+        selectStar.click();
+        Thread.sleep(3000);
+
+        List<WebElement> titleOfCoffeemug = driver.findElements(By.xpath("//a[@class='wjcEIp']"));
+        List<String> title = new  ArrayList<String>();
+        for (WebElement e : titleOfCoffeemug)
+        {
+            title.add(e.getText());
+        }
+        List<String> newTitle = new ArrayList<String>(title);
+
+        List<WebElement> imgUrlList = driver.findElements(By.xpath("//img[@class='DByuf4']"));
+        List<String> imGUrl = new  ArrayList<String>();
+        for (WebElement e : imgUrlList)
+        {
+            imGUrl.add(e.getText());
+        }
+        List<String> newImgUrl = new ArrayList<String>(imGUrl);
+
+        List<WebElement> reviewList = driver.findElements(By.xpath("//span[@class='Wphh3N']"));
+        List<String> reviewLst = new  ArrayList<String>();
+        for (WebElement e : reviewList)
+        {
+            reviewLst.add(e.getText());
+        }
+        List<String> newReview = new ArrayList<String>(reviewLst);
+        int largestString = newReview.get(0).length();
+        for (int i = 0; i <= 5 ; i++) {
+            if(newReview.get(i).length() > largestString){
+                System.out.println("title of coffeemug is : "+newTitle.get(i));
+                System.out.println("img url of coffeemug is : "+newImgUrl.get(i));
+            }
+        }
+        System.out.println("End Test case: testCase03");
+    }
     @AfterTest
     public void endTest()
     {
